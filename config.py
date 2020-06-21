2f720f447f9d3a8ff01de5d1099d3bbcfe1e4003
danghuutoan <danghuutoan@gmail.com>
2020-06-21 13:20:04 +0700

    update database url

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/casting_agency'
+# SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/casting_agency'
+SQLALCHEMY_DATABASE_URI = 'postgres://xnosmblujpcipz:36607d3553168132f34ae90933b8629522a941f305e1094b6bdb6e787aa67122@ec2-52-72-221-20.compute-1.amazonaws.com:5432/ddj54gsu1fq5dm'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
