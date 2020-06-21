3deb515ade86e5bef254af88a4cbed7e28bf1678
danghuutoan <danghuutoan@gmail.com>
2020-06-21 13:42:46 +0700

    read db uri from env variable

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ DEBUG = True
 
 # TODO IMPLEMENT DATABASE URL
 # SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost:5432/casting_agency'
-SQLALCHEMY_DATABASE_URI = 'postgres://xnosmblujpcipz:36607d3553168132f34ae90933b8629522a941f305e1094b6bdb6e787aa67122@ec2-52-72-221-20.compute-1.amazonaws.com:5432/ddj54gsu1fq5dm'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
